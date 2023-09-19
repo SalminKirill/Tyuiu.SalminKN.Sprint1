07b238c735c5dbc381836871bf6d89805c19b709
Tyuiu.SalminKN@outlook.com <Tyuiu.SalminKN@outlook.com>
2023-09-19 21:20:54 +0500

    Add project files.

---
+++ Tyuiu.SalminKN.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
+using Tyuiu.SalminKN.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.SalminKN.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            var res = ds.Calculate();
+
+            Assert.AreEqual(2, res);
+
+        }
+    }
+}
---
+++ Tyuiu.SalminKN.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SalminKN.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.SalminKN.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Салмин К. Н. | АСОиУБ-23-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #1                                                            *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                     *");
+            Console.WriteLine("* Задание #0                                                           *");
+            Console.WriteLine("* Вариант #0                                                           *");
+            Console.WriteLine("* Выполнил: Салмин Кирилл Николаевич | АСОиУБ-23-1                     *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)         *");
+            Console.WriteLine("* и печатает результат на экране.                                      *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* 10 / (2 + 3)                                                         *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+        }
+    }
+}
