b23407ffb3605d9a5c4200cf195c870479c8bdce
Tyuiu.SalminKN@outlook.com <Tyuiu.SalminKN@outlook.com>
2023-09-19 21:46:44 +0500

    Task0.V13

---
+++ Tyuiu.SalminKN.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SalminKN.Sprint1.Task0.V13.Lib;
+
+namespace Tyuiu.SalminKN.Sprint1.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            var res = ds.Calculate();
+
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
---
+++ Tyuiu.SalminKN.Sprint1.Task0.V13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SalminKN.Sprint1.Task0.V13.Lib;
+
+namespace Tyuiu.SalminKN.Sprint1.Task0.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Салмин К. Н. | АСОиУБ-23-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #1                                                            *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                     *");
+            Console.WriteLine("* Задание #1.0                                                         *");
+            Console.WriteLine("* Вариант #1                                                           *");
+            Console.WriteLine("* Выполнил: Салмин Кирилл Николаевич | АСОиУБ-23-1                     *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 24/(6*2)-24/6/4      *");
+            Console.WriteLine("* и печатает результат на экране.                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* 24 / (6 * 2) - 24 / 6 / 4                                            *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+        }
+    }
+}
