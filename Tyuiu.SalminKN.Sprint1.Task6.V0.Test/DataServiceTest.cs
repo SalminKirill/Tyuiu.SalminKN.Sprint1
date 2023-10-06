18de4a71c01a8140b2aeac68645171c0c631ef14
Tyuiu.SalminKN@outlook.com <Tyuiu.SalminKN@outlook.com>
2023-10-06 22:39:06 +0500

    Task6

---
+++ Tyuiu.SalminKN.Sprint1.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SalminKN.Sprint1.Task6.V0.Lib;
+namespace Tyuiu.SalminKN.Sprint1.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string strTest = "1 * 2 * 3";
+            DataService ds = new DataService();
+            string res = ds.WorkWithText(strTest);
+            string wait = "123";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SalminKN.Sprint1.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SalminKN.Sprint1.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SalminKN.Sprint1.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fba67b25-2f68-4b81-8be5-d8097acf1cd5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
